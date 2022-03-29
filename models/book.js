a9d726ccc5374f96dc8e8728a7f75e1b17377a45
Katrina Lenell Hill <khill365@gmail.com>
2022-03-29 13:56:44 -0700

    made schema lower case b

---
+++ models/book.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// bring in mongoose
+const mongoose = require('mongoose');
+
+// extract schema property from the mongoose object
+const { Schema } = mongoose;
+
+// create a book schema, define how our object will be structured
+const bookSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    status: {type: Boolean, required: true},
+    email: {type: String, required: true}
+  });
+
+const BookModel = mongoose.model('book', bookSchema);
+
+module.exports = BookModel;
